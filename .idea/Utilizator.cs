9b491db649e67c21950590746d287f5f1e531b99
maraaa3 <mara.broscatan@yahoo.com>
2024-12-27 17:27:28 +0200

    utilizator

--- .idea/Utilizator.cs
+++ .idea/Utilizator.cs
@@ -1,6 +1,21 @@
 namespace DefaultNamespace;
 
-public class Utilizator
+public abstract class Utilizator
 {
-    public string CodUnic { get; }
+    public string CodUnic { get;  }
+    public string Nume { get;  }
+    public string Prenume { get;  }
+    public string Email { get;  }
+    public string Parola { get;  }
+
+    protected Utilizator(string codUnic, string nume, string prenume, string email, string parola)
+    {
+        CodUnic = codUnic;
+        Nume = nume;
+        Prenume = prenume;
+        Email = email;
+        Parola = parola;
+    }
+
+    public abstract void Meniu();
 }
