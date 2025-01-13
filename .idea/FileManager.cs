485e225bcde7cf771bea321bd20f0f1ac39fdf02
maraaa3 <mara.broscatan@yahoo.com>
2025-01-13 21:19:39 +0200

    curatare cod

--- .idea/FileManager.cs
+++ .idea/FileManager.cs
@@ -78,33 +78,5 @@ public class FileManager
             return new List<T>();
         }
     }
-/*private readonly FileManager _cereriRezolvareFileManager = new FileManager("cereri_rezolvare.json");
 
-public void SalveazaCereriRezolvare()
-{
-    _cereriRezolvareFileManager.SaveToFile(_cereriRezolvare);
-} //cred
-
-private List<CerereRezolvare> _cereriRezolvare = new List<CerereRezolvare>();
-
-public void IncarcaCereriRezolvare()
-{
-    _cereriRezolvare = _cereriRezolvareFileManager.LoadFromFile<CerereRezolvare>();
-}//cred
-
-	private List<CererePieseAuto> _cereriPieseAuto = new List<CererePieseAuto>();
-
-    public void SalveazaCereriPieseAuto()
-    {
-        var fileManager = new FileManager("cereri_piese_auto.json");
-        fileManager.SaveToFile(_cereriPieseAuto);
-    }
-
-    // Încarcă cererile de piese auto
-    public void IncarcaCereriPieseAuto()
-    {
-        var fileManager = new FileManager("cereri_piese_auto.json");
-        _cereriPieseAuto = fileManager.LoadFromFile<CererePieseAuto>();
-    }
-    //SA FAC IN PROGRAM FISIERE PT FIECARE*/
 }
